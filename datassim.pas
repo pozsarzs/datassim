2e7fa8ccc60a14ab7c904c734a41b470e7bc8c47
Pozsar Zsolt <pozsarzs@karak.lan>
2025-06-21 10:58:18 +0200

    250621

--- datassim.pas
+++ datassim.pas
@@ -24,140 +24,160 @@ type
   end;
 var
   b:            byte;
-  bp:           byte;                          { breakpoint address}
-  com:          TCommand;                      { command line content}
-  mem:          array[0..99] of TMemcell;      { 'memory' }
-  pc:           byte;                          { program counter }
+  bp:           byte;                                     { breakpoint address }
+  com:          TCommand;                               { command line content }
+  mem:          array[0..99] of TMemcell;                           { 'memory' }
+  pc:           byte;                                        { program counter }
   q:            boolean;
-  splitted:     array[0..7] of TSplitted;      { splitted command }
   s:            string[255];
+  splitted:     array[0..7] of TSplitted;                   { splitted command }
 const
   COMMARRSIZE = 14;
   COMMANDS:     array[0..COMMARRSIZE] of string[7] = (
-                'break',  'comment','deposit','examine','export', 'fill',
-                'help',   'import', 'load',   'quit',   'reset',  'run',
-                'save',   'step',   'dump');
-  COMMANDS_INF: array[0..1,0..COMMARRSIZE] of string[32] = ((
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                ''),(
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                '',
-                ''));
+                'break',  'comment','deposit','dump',   'examine','export',
+                'fill',   'help',   'import', 'load',   'quit',   'reset',
+                'run',    'save',   'step');
+  COMMANDS_INF: array[0..1,0..COMMARRSIZE] of string[63] = ((
+                'set and reset breakpoint at AA address',
+                'add or remove a sigle-line note for program or AA address',
+                'deposit D1-4 value at AA address',
+                'print the value of memory cell number CN from AA',
+                'examine value at AA address',
+                'export memory content to a binary file',
+                'fill the memory cell number CN from AA with the value D1-4',
+                'help with using the program',
+                'import memory content from a binary file',
+                'load source code',
+                'exit the simulator program',
+                'reset simulator',
+                'run program from AA address',
+                'save source code',
+                'run program step-by-step from AA address'),(
+                'break [AA]',
+                'comment [My\ new\ program] | AA [Jump\ to\ 12]',
+                'deposit AA D1 D2 D3 D4',
+                'dump AA CN',
+                'examine AA',
+                'export filename.bin',
+                'fill AA CN D1 D2 D3 D4',
+                'help [command]',
+                'import filename.bin',
+                'load filename.lst',
+                'quit',
+                'reset',
+                'run [AA]',
+                'save filename.lst',
+                'step'));
   COMMENT =     ';';
   HEADER1 =     'DATASSim v0.1 for CP/M and DOS';
   HEADER2 =     '(C) 2025 Pozsar Zsolt <pozsarzs@gmail.com>';
   HEADER3 =     'Licence: EUPL v1.2';
   HINT =        'Type "h" for useable commands.';
-  MESSAGE:      array[0..12] of string[32] = (
+  MESSAGE:      array[0..11] of string[39] = (
                 'No such command!',
-                'm1',
-                'm2',
-                'm3',
-                'm4',
-                'm5',
-                'm6',
-                'm7',
-                'm8',
-                'm9',
-                'm10',
-                'm11',
-                'm12');
+                ' 1st ',
+                ' 2nd ',
+                ' 3rd ',
+                ' 4th ',
+                ' 5th ',
+                ' 6th ',
+                'The',
+                'parameter is incorrect.',
+                'parameter value is incorrect: ',
+                'The breakpoint is deleted.',
+                'The breakpoint is set to this address: ');
   PROMPT =      'SIM>';
 
-{ set and reset breakpoint }
-{ parameters: AA or }
-procedure cmd_break(p1: Tsplitted);
-begin
-end;
-
-{ set comment }
-{ 'comment' or AA 'comment' }
-procedure cmd_comment(p1, p2: Tsplitted);
-begin
-end;
-
-{ deposit data in memory }
-{ parameters: AA D1 D2 D3 D4}
-procedure cmd_deposit(p1, p2, p3, p4, p5: Tsplitted);
-begin
-end;
-
-{ examine data in memory }
-{ parameters: AA D1 D2 D3 D4}
-procedure cmd_examine;
-begin
-end;
-{ parameters: AA D1 D2 D3 D4}
-procedure cmd_export;
-begin
-end;
-
-{ fill memory with data }
-{ parameters: AA count D1 D2 D3 D4}
-procedure cmd_fill(p1, p2, p3, p4, p5, p6: Tsplitted);
+{ COMMAND 'break' }
+procedure cmd_break(p1: TSplitted);
 var
-  ip1, ip2, ip3, ip4, ip5, ip6, ec: integer;
-  b: byte;
-  e: byte;
+  e:   byte;
+  ec:  integer;
+  ip1: integer;
+begin
+  if length(p1) = 0 then
+  begin
+    bp := 255;
+    writeln(MESSAGE[10])
+  end else
+  begin
+    val(p1, ip1, ec);
+    if ec = 0
+    then
+      if (ip1 >= 0) and (ip1 <= 99) then e := 0 else e := 11
+    else e := 12;
+    case e of
+      11: writeln(MESSAGE[7]+MESSAGE[1]+MESSAGE[8]);
+      12: writeln(MESSAGE[7]+MESSAGE[1]+MESSAGE[9]+'[0-99]');
+    else
+      begin
+        bp := ip1;
+        writeln(MESSAGE[11])
+      end;
+    end;
+  end;
+end;
+
+{ COMMAND 'comment' }
+procedure cmd_comment(p1, p2: TSplitted);
+begin
+end;
+
+{ COMMAND 'deposit' }
+procedure cmd_deposit(p1, p2, p3, p4, p5: TSplitted);
+begin
+end;
+
+{ COMMAND 'dump' }
+procedure cmd_dump(p1, p2: TSplitted);
+begin
+end;
+
+{ COMMAND 'examine' }
+procedure cmd_examine(p1: TSplitted);
+begin
+end;
+
+{ COMMAND 'export' }
+procedure cmd_export(p1: TSplitted);
+begin
+end;
+
+{ COMMAND 'fill' }
+procedure cmd_fill(p1, p2, p3, p4, p5, p6: TSplitted);
+var
+  ip1, ip2, ip3, ip4, ip5, ip6: integer;
+  ec:                           integer;
+  b:                            byte;
+  e:                            byte;
 begin
   { check parameters }
   val(p1, ip1, ec);
-  writeln(ip1);
   if ec = 0
   then
     if (ip1 >= 0) and (ip1 <= 99) then
     begin
       val(p2, ip2, ec);
-      writeln(p2);
       if ec = 0
       then
         if (ip2 >= 1) and (ip2 <= 100) then
         begin
           val(p3, ip3, ec);
-          writeln(ip3);
           if ec = 0
           then
             if (ip3 >= 0) and (ip3 <= 99) then
             begin
               val(p4, ip4, ec);
-              writeln(ip4);
               if ec = 0
               then
                 if (ip4 >= 0) and (ip4 <= 99) then
                 begin
                   val(p5, ip5, ec);
-                  writeln(ip5);
                   if ec = 0
                   then
                     if (ip5 >= 0) and (ip5 <= 99) then
                     begin
                       val(p6, ip6, ec);
-                      writeln(ip6);
                       if ec = 0
                       then
                         if (ip6 >= 0) and (ip6 <= 99) then e := 0 else e := 61
@@ -173,24 +193,23 @@ begin
     end else e := 11
   else e := 12;
   case e of
-    11: writeln(MESSAGE[1]);
-    12: writeln(MESSAGE[2]);
-    21: writeln(MESSAGE[3]);
-    22: writeln(MESSAGE[4]);
-    31: writeln(MESSAGE[5]);
-    32: writeln(MESSAGE[6]);
-    41: writeln(MESSAGE[7]);
-    42: writeln(MESSAGE[8]);
-    51: writeln(MESSAGE[9]);
-    52: writeln(MESSAGE[10]);
-    61: writeln(MESSAGE[11]);
-    62: writeln(MESSAGE[12]);
+    11: writeln(MESSAGE[7]+MESSAGE[1]+MESSAGE[8]);
+    12: writeln(MESSAGE[7]+MESSAGE[1]+MESSAGE[9]+'[0-99]');
+    21: writeln(MESSAGE[7]+MESSAGE[2]+MESSAGE[8]);
+    22: writeln(MESSAGE[7]+MESSAGE[2]+MESSAGE[9]+'[1-100]');
+    31: writeln(MESSAGE[7]+MESSAGE[3]+MESSAGE[8]);
+    32: writeln(MESSAGE[7]+MESSAGE[3]+MESSAGE[9]+'[0-99]');
+    41: writeln(MESSAGE[7]+MESSAGE[4]+MESSAGE[8]);
+    42: writeln(MESSAGE[7]+MESSAGE[4]+MESSAGE[9]+'[0-99]');
+    51: writeln(MESSAGE[7]+MESSAGE[5]+MESSAGE[8]);
+    52: writeln(MESSAGE[7]+MESSAGE[5]+MESSAGE[9]+'[0-99]');
+    61: writeln(MESSAGE[7]+MESSAGE[6]+MESSAGE[8]);
+    62: writeln(MESSAGE[7]+MESSAGE[6]+MESSAGE[9]+'[0-99]');
   else
     begin
       for b := ip1 to ip1 + ip2 do
         if (ip1 + ip2) <= 99 then
         begin
-          writeln(b);
           mem[b].data[0] := ip3;
           mem[b].data[1] := ip4;
           mem[b].data[2] := ip5;
@@ -200,51 +219,39 @@ begin
   end;
 end;
 
-{ parameters: AA D1 D2 D3 D4}
-procedure cmd_help;
+{ COMMAND 'help' }
+procedure cmd_help(p1: TSplitted);
 begin
 end;
-{ parameters: AA D1 D2 D3 D4}
-procedure cmd_import;
+
+{ COMMAND 'import' }
+procedure cmd_import(p1: TSplitted);
 begin
 end;
-{ parameters: AA D1 D2 D3 D4}
-procedure cmd_load;
+
+{ COMMAND 'load' }
+procedure cmd_load(p1: TSplitted);
 begin
 end;
-{ parameters: AA D1 D2 D3 D4}
-procedure cmd_quit;
-begin
-end;
-{ parameters: AA D1 D2 D3 D4}
+
+{ COMMAND 'reset' }
 procedure cmd_reset;
 begin
 end;
-{ parameters: AA D1 D2 D3 D4}
-procedure cmd_run;
-begin
-end;
-{ parameters: AA D1 D2 D3 D4}
-procedure cmd_save;
+
+{ COMMAND 'run' }
+procedure cmd_run(p1: TSplitted);
 begin
 end;
 
-{ parameters: AA D1 D2 D3 D4}
-procedure cmd_step;
+{ COMMAND 'save' }
+procedure cmd_save(p1: TSplitted);
 begin
 end;
 
-{ dump memory content }
-{ parameters: AA count }
-procedure cmd_dump;
-var
-  b: byte;
+{ COMMAND 'step' }
+procedure cmd_step(p1: TSplitted);
 begin
-  for b:= 0 to 99 do
-  begin
-    write(b, '|',mem[b].data[0],'  ',mem[b].data[1],'  ',mem[b].data[2],'  ',mem[b].data[3]);
-    writeln;
-  end;
 end;
 
 { PARSING COMMANDS }
@@ -259,70 +266,80 @@ begin
   parsingcommand := false;
   if (length(command) > 0) then
   begin
-      { remove space and tab from start of line }
-      while (command[1] = #32) or (command[1] = #9) do
-        delete(command, 1, 1);
-      { remove space and tab from end of line }
-      while (command[length(command)] = #32) or
-            (command[length(command)] = #9) do
-        delete(command, length(command), 1);
-      { remove extra space and tab from line }
-      for b := 1 to 255 do
-      begin
-        if b = length(command) then goto break1;
-        if command[b] <> #32 then o := false;
-        if (command[b] = #32) and o then command[b] :='@';
-        if command[b] = #32 then o := true;
-      end;
-    break1:
-      s := '';
-      for b := 1 to length(command) do
-        if command[b] <> '@' then s := s + command[b];
-      command := s;
-      { split command to 8 slices }
-      for b := 0 to 7 do
-        splitted[b] := '';
-      for a := 1 to length(command) do
+    { remove space and tab from start of line }
+    while (command[1] = #32) or (command[1] = #9) do
+      delete(command, 1, 1);
+    { remove space and tab from end of line }
+    while (command[length(command)] = #32) or (command[length(command)] = #9) do
+      delete(command, length(command), 1);
+    { remove extra space and tab from line }
+    for b := 1 to 255 do
+    begin
+      if b = length(command) then goto break1;
+      if command[b] <> #32 then o := false;
+      if (command[b] = #32) and o then command[b] :='@';
+      if command[b] = #32 then o := true;
+    end;
+  break1:
+    s := '';
+    for b := 1 to length(command) do
+      if command[b] <> '@' then s := s + command[b];
+    command := s;
+    { split command to 8 slices }
+    for b := 0 to 7 do
+      splitted[b] := '';
+    for a := 1 to length(command) do
+      if (command[a] = #32) and (command[a - 1] <> #92)
+        then goto break2
+        else splitted[0] := splitted[0] + command[a];
+  break2:
+    for b:= 1 to 7 do
+    begin
+      for a := a + 1 to length(command) do
         if (command[a] = #32) and (command[a - 1] <> #92)
-          then goto break2
-          else splitted[0] := splitted[0] + command[a];
-    break2:
-      for b:= 1 to 7 do
-      begin
-        for a := a + 1 to length(command) do
-          if (command[a] = #32) and (command[a - 1] <> #92)
-            then goto break3
-            else splitted[b] := splitted[b] + command[a];
-        break3:
-      end;
-      { parse command }
-      o := false;
-      if splitted[0][1] <> COMMENT then
-      begin
-        for b := 0 to COMMARRSIZE do
-          if splitted[0] = COMMANDS[b] then
-          begin
-            o := true;
-            goto break4;
-          end;
-      break4:
-          writeln(splitted[1], splitted[2], splitted[3], splitted[4], splitted[5], splitted[6]);
-        if o then
+          then goto break3
+          else splitted[b] := splitted[b] + command[a];
+    break3:
+    end;
+    { parse command }
+    o := false;
+    if splitted[0][1] <> COMMENT then
+    begin
+      for b := 0 to COMMARRSIZE do
+        if splitted[0] = COMMANDS[b] then
         begin
-          case b of
-              0: cmd_help;
-              5: cmd_fill(splitted[1], splitted[2], splitted[3],
-                          splitted[4], splitted[5], splitted[6]);
-              9: parsingcommand := true;
-              14: cmd_dump;
+          o := true;
+          goto break4;
         end;
-      end else
-        writeln(MESSAGE[0]);
-   end;
+    break4:
+      if o then
+      begin
+        case b of
+           0: cmd_break(splitted[1]);
+           1: cmd_comment(splitted[1], splitted[2]);
+           2: cmd_deposit(splitted[1], splitted[2], splitted[3],
+                          splitted[4], splitted[5]);
+           3: cmd_dump(splitted[1], splitted[2]);
+           4: cmd_examine(splitted[1]);
+           5: cmd_export(splitted[1]);
+           6: cmd_fill(splitted[1], splitted[2], splitted[3],
+                       splitted[4], splitted[5], splitted[6]);
+           7: cmd_help(splitted[1]);
+           8: cmd_import(splitted[1]);
+           9: cmd_load(splitted[1]);
+          10: parsingcommand := true;
+          11: cmd_reset;
+          12: cmd_run(splitted[1]);
+          13: cmd_save(splitted[1]);
+          14: cmd_step(splitted[1]);
+        end;
+      end else writeln(MESSAGE[0]);
+    end;
   end;
 end;
 
 begin
+  { show information and command line hint}
   writeln(HEADER1);
   writeln(HEADER2);
   writeln(HEADER3);
@@ -330,10 +347,11 @@ begin
   writeln;
   writeln(HINT);
   { initialize memory, breakpoint and program counter }
-  parsingcommand('fill 00 100 00 00 00 00');
+  q := parsingcommand('fill 00 100 00 00 00 00');
   bp := 255;
   pc := 0;
   q := false;
+  { main operation }
   repeat
     write(PROMPT); readln(com);
     q := parsingcommand(com);
---
+++ lst2bin.pas
@@ -0,0 +1,126 @@
+{ +--------------------------------------------------------------------------+ }
+{ | DATASSim * Simulator for DATAS machine code                              | }
+{ | Copyright (C) 2025 Pozsar Zsolt <pozsarzs@gmail.com>                     | }
+{ | lst2bin.pas                                                              | }
+{ | Source list to binary file converter (Turbo Pascal 3.0 CP/M and DOS)     | }
+{ +--------------------------------------------------------------------------+ }
+{
+  This program is free software: you can redistribute it and/or modify it
+  under the terms of the European Union Public License 1.2 version.
+
+  This program is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+  FOR A PARTICULAR PURPOSE.
+}
+
+program lst2bin;
+type
+  TFilename =    string[12];
+  TMemcell=      record
+    data:        array[0..3] of byte;
+  end;
+var
+  address, data: byte;
+  error:         byte;
+  line:          byte;
+  memory:        array[0..99] of TMemcell;
+
+{ LOAD LST FILE TO MEMORY }
+function lst2mem(filename: TFilename): byte;
+var
+  address: byte;
+  b:       byte;
+  i1, i2:  integer;
+  ec:      integer;
+  lstfile: text;
+  s1, s2:  string[255];
+begin
+  assign(lstfile, filename);
+  {$I-}
+    reset(lstfile);
+  {$I+}
+  if ioresult <> 0 then
+  begin
+    lst2mem := 1;
+    exit;
+  end;
+  line := 0;
+  repeat
+    readln(lstfile, s1);
+    line := line + 1;
+    s2 := '';
+    { remove all space and tabulator }
+    for b := 1 to length(s1) do
+      if (s1[b] <> #32) and (s1[b] <> #9) then s2 := s2 + s1[b];
+    { check comment sign }
+    if s2[1] <> ';' then
+    begin
+      { check length of line }
+      if length(s2) < 10 then
+      begin
+        lst2mem := 3;
+        close(lstfile);
+        exit;
+      end;
+      { check content }
+      for b := 1 to 10 do
+        if not ((s2[b] >= #48) and (s2[b] <= #57)) then
+        begin
+          lst2mem := 4;
+          close(lstfile);
+          exit;
+        end;
+      { load to memory }
+      val(s2[1] + s2[2], i1, ec);
+      address := i1;
+      for b := 0 to 3 do
+      begin
+        val(s2[3 + b * 2] + s2[4 + b * 2], i2, ec);
+        memory[address].data[b]:= i2;
+      end;
+    end;
+  until eof(lstfile);
+  close(lstfile);
+  lst2mem := 0;
+end;
+
+{ SAVE BINARY FILE FROM MEMORY }
+function mem2bin(filename: TFilename): byte;
+var
+  binfile: file of byte;
+begin
+  assign(binfile, filename);
+  {$I-}
+    rewrite(binfile);
+  {$I+}
+  if ioresult <> 0 then
+  begin
+    mem2bin := 2;
+    exit;
+  end;
+  for address := 0 to 99 do
+    for data := 0 to 3 do
+      write(binfile, memory[address].data[data]);
+  close(binfile);
+  mem2bin := 0;
+end;
+
+begin
+  error := 0;
+  line := 0;
+  if paramcount > 1 then
+  begin
+    for address := 0 to 99 do
+      for data := 0 to 3 do
+        memory[address].data[data] := 0;
+    error := lst2mem(paramstr(1));
+    if error = 0 then error := mem2bin(paramstr(2));
+    case error of
+      1: writeln('File read error!');
+      2: writeln('File write error!');
+      3: writeln('Too short line: ', line);
+      4: writeln('Illegal character in: ', line) ;
+    end;
+  end else writeln('Usage lst2bin.com input.lst output.bin');
+  halt(error);
+end.
